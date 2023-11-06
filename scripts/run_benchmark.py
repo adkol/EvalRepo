1a5559316f6adcd5252c4526cb95addcc9255549
adkolli <aditkk29@gmail.com>
2023-11-05 23:56:34 -0500

    Trying to get rid of modulenotefound

--- scripts/run_benchmark.py
+++ scripts/run_benchmark.py
@@ -2,7 +2,7 @@
 #python train.py  --method=GP_BOTORCH --benchmark=sysbench --workload_type=readwrite --knobs_config=mysql_all_cdbtune.json --dbname=sbtest --lhs_log=xxx.res
 from pathlib import Path
 import sys
-path_root = Path(__file__).parents[2]
+path_root = Path(__file__).parents[1]
 sys.path.append(str(path_root))
 print(sys.path)
 
